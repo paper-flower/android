5234bd2f3fd0461243be0a3cc8618c794e5360db
RhPlus <x1wins@naver.com>
2018-06-19 01:54:14 +0900

    add ArticleClient

---
+++ app/src/main/java/net/changwoo/rhplus/paper_flower_android/http/ArticleClient.java
@@ -0,0 +1,22 @@
+package net.changwoo.rhplus.paper_flower_android.http;
+
+public class ArticleClient extends RestApiClient {
+    public ArticleClient(){
+        super();
+    }
+    public void getArticle(){
+
+    }
+    public void getTimeline(Integer id){
+
+    }
+    public void deleteTimeline(Integer id){
+
+    }
+    public void postArticle(){
+
+    }
+    public void updateArticle(){
+
+    }
+}
