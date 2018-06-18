4639aa4340f6f4cc4462a8e4bf0fd33885536a06
RhPlus <x1wins@naver.com>
2018-06-19 01:53:59 +0900

    add abstract

--- app/src/main/java/net/changwoo/rhplus/paper_flower_android/http/RestApiClient.java
+++ app/src/main/java/net/changwoo/rhplus/paper_flower_android/http/RestApiClient.java
@@ -1,27 +1,20 @@
 package net.changwoo.rhplus.paper_flower_android.http;
 
+import retrofit2.Retrofit;
+
 /**
  * Created by rhplus on 19/06/2018.
  */
 
-public class RestApiClient {
+public abstract class RestApiClient {
     private static String baseUrl;
-//    private static Retrofit retrofit;
+    protected static Retrofit retrofit;
 
     private static RestApiClient instance;
-    private RestApiClient () {}
-
-    public static RestApiClient getInstance () {
-        if ( instance == null ){
-            instance = new RestApiClient();
-            baseUrl = "https://api.github.com/";
-//            retrofit = new Retrofit.Builder()
-//                    .baseUrl(baseUrl)
-//                    .build();
-
-        }
-        return instance;
+    public RestApiClient () {
+        baseUrl = "https://api.github.com/";
+        retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .build();
     }
-
-
 }
